2dd4f456b12ac0a4f6d036a2dbe608647b87334f
Jesse Cahill <jcahill225@gmail.com>
2018-03-22 22:30:23 -0400

    Modified scrape test to use function definitions and catch errors

--- scrapetest.py
+++ scrapetest.py
@@ -1,3 +1,24 @@
 from urllib.request import urlopen
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+from urllib.error import HTTPError, URLError
+from bs4 import BeautifulSoup
+
+
+def getTitle(url):
+    try:
+        html = urlopen("http://pythonscraping.com/pages/page1.html")
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+if __name__ == "__main__":
+    title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+    if title is None:
+        print("Title could not be found")
+    else:
+        print(title)
