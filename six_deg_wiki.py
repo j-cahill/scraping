2a58b81753e3c49eb2a291b0ad834424cc5509a3
Jesse Cahill <jcahill225@gmail.com>
2018-03-27 23:29:31 -0400

    addedduplicate link checking

--- six_deg_wiki.py
+++ six_deg_wiki.py
@@ -1,10 +1,31 @@
-import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import datetime
+import random
+import re
 
-html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
-soup = BeautifulSoup(html)
-for link in soup.find("div", {"id":"bodyContent"}).findAll("a",
-                                                         href=re.compile("^(/wiki/)((?!:).)*$")):
-    if "href" in link.attrs:
-        print(link.attrs['href'])
+random.seed(datetime.datetime.now())
+
+pages = set()
+def get_links(page_url):
+    global pages
+    html = urlopen('http://en.wikipedia.org/' + page_url)
+    soup = BeautifulSoup(html)
+    for link in soup.findAll('a', href=re.compile('^(/wiki/)')):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                # We have encountered a new page
+                new_page = link.attrs['href']
+                print(new_page)
+                pages.add(new_page)
+                get_links(new_page)
+
+get_links("")
+
+"""    
+links = get_links('/wiki/Kevin_Bacon')
+while links:
+    new_article = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(new_article)
+    links = get_links(new_article)
+"""
